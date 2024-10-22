071c634bfc1f647ef7e2054c8de003e58bc64c5c
jeanheberth <jeanheberth19@gmail.com>
2024-10-22 10:24:18 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {MusicComponent} from './music/music.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
+  imports: [
+    MusicComponent
+  ],
+  template: '<app-music></app-music>',
+  // templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
